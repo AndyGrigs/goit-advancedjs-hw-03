8fed30bbc2b1e47bd64c089983989c2439fd3e06
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2025-03-16 13:26:41 +0100

    added axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,20 @@
 
+
+import axios from "axios";
+
 const BASE_URL = "https://pixabay.com/api/";
-const API = "28460995-5acfdb805ab0c27f2bf717abb";
+const API_KEY = "28460995-5acfdb805ab0c27f2bf717abb"; 
+
 
 export async function loadImages(query) {
-    const params = new URLSearchParams({
-        key: API,
-        q: query,
-        image_type: "photo",
-        orientation: "horizontal",
-    });
-
-    const response = await fetch(`${BASE_URL}?${params}`);
-    if(!response.ok){
-        throw new Error(response.statusText);
-    }
-    return await response.json();
-    
-} 
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: "true",
+  });
+  
+  const response = await axios.get(`${BASE_URL}?${params}`);
+  return response.data;
+}
